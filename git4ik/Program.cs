d7706bda5f3ea1fcbc705f5a209d16174d11ea07
Вадим Комиссаров <komissarovadim@gmail.com>
2022-10-13 07:44:03 +0300

    sdf

--- git4ik/Program.cs
+++ git4ik/Program.cs
@@ -1,11 +1,5 @@
 ﻿class Program
 {
-    static void wrt(double x, double y) => Console.WriteLine($"{x}, {y}");
-    static void sum(double x, double y) => Console.WriteLine(x + y);
-    static void sub(double x, double y) => Console.WriteLine(x - y);
-    static void div(double x, double y) => Console.WriteLine(x / y);
-    static void pow(double x, double y) => Console.WriteLine(Math.Pow(x, y));
-
     static void Main(string[] args)
     {
         bool tf = true;
