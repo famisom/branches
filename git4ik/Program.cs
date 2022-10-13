6aa1967ce7aa9d5228874523adcddcb4165aaebd
Вадим Комиссаров <komissarovadim@gmail.com>
2022-10-13 07:34:38 +0300

    Добавьте файлы проекта.

---
+++ git4ik/Program.cs
@@ -0,0 +1,56 @@
+﻿class Program
+{
+    static void wrt(double x, double y) => Console.WriteLine($"{x}, {y}");
+    static void sum(double x, double y) => Console.WriteLine(x + y);
+    static void sub(double x, double y) => Console.WriteLine(x - y);
+    static void div(double x, double y) => Console.WriteLine(x / y);
+    static void pow(double x, double y) => Console.WriteLine(Math.Pow(x, y));
+
+    static void Main(string[] args)
+    {
+        bool tf = true;
+
+        Console.WriteLine("Введите 2 числа:");
+        double x = Convert.ToDouble(Console.ReadLine());
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        while (tf)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Меню:" +
+                "\n1. Вывести числа" +
+                "\n2. Найти сумму" +
+                "\n3. Найти разность" +
+                "\n4. Найти частное" +
+                "\n5. Возвести в степень" +
+                "\n6. Выйти");
+
+            int res = Convert.ToInt32(Console.ReadLine());
+
+            switch (res)
+            {
+                case 1:
+                    wrt(x, y);
+                    break;
+                case 2:
+                    sum(x, y);
+                    break;
+                case 3:
+                    sub(x, y);
+                    break;
+                case 4:
+                    div(x, y);
+                    break;
+                case 5:
+                    pow(x, y);
+                    break;
+                default:
+                    tf = false;
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
